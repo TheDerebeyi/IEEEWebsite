346073c7e296293efd44bdeb7cad6891f32db004
Mert Kahraman <rehber.tadinda1@gmail.com>
2021-07-24 16:00:18 +0300

    test

--- JS/indexEvents.js
+++ JS/indexEvents.js
@@ -1,48 +1,53 @@
-var obj = fetch("./Datas/Activities/2021.json").then(response => response.json());
+async function foo() {
 
-alert(obj[0].id);
+    var source = await fetch("./Datas/Activities/2021.json");
 
-var arr = [];
+    var obj = await source.json();
 
-var counter = 0;
+    alert(data[0].id);
 
-for (let i = 0; i < obj.length; i++) {
-    let date1 = new Date(obj[i].date);
-    let date2 = new Date();
+    var arr = [];
 
-    if (date1.getUTCFullYear() - date2.getUTCFullYear() > 0) {
-        arr[counter] = i;
-        counter++;
-    }
-    else if (date1.getUTCFullYear() - date2.getUTCFullYear() == 0) {
-        if (date1.getUTCMonth() - date2.getUTCMonth() > 0) {
+    var counter = 0;
+
+    for (let i = 0; i < obj.length; i++) {
+        let date1 = new Date(obj[i].date);
+        let date2 = new Date();
+
+        if (date1.getUTCFullYear() - date2.getUTCFullYear() > 0) {
             arr[counter] = i;
             counter++;
         }
-        else if (date1.getUTCMonth() - date2.getUTCMont == 0) {
+        else if (date1.getUTCFullYear() - date2.getUTCFullYear() == 0) {
             if (date1.getUTCMonth() - date2.getUTCMonth() > 0) {
                 arr[counter] = i;
                 counter++;
             }
-            else if (date1.getUTCMonth() - date2.getUTCMonth() == 0) {
-                if (date1.getUTCDay() - date2.getUTCDay() > 0) {
+            else if (date1.getUTCMonth() - date2.getUTCMont == 0) {
+                if (date1.getUTCMonth() - date2.getUTCMonth() > 0) {
                     arr[counter] = i;
                     counter++;
                 }
-                else if (date1.getUTCDay() - date2.getUTCDay() == 0) {
-                    if (date1.getUTCHours() - date2.getUTCHours() > 0) {
+                else if (date1.getUTCMonth() - date2.getUTCMonth() == 0) {
+                    if (date1.getUTCDay() - date2.getUTCDay() > 0) {
                         arr[counter] = i;
                         counter++;
                     }
-                    else if (date1.getUTCHours() - date2.getUTCHours() == 0) {
-                        if (date1.getUTCMinutes() - date2.getUTCMinutes() > 0) {
+                    else if (date1.getUTCDay() - date2.getUTCDay() == 0) {
+                        if (date1.getUTCHours() - date2.getUTCHours() > 0) {
                             arr[counter] = i;
                             counter++;
                         }
-                        else if (date1.getUTCMinutes() - date2.getUTCMinutes() == 0) {
-                            if (date1.getUTCSeconds() - date2.getUTCSeconds() > 0) {
+                        else if (date1.getUTCHours() - date2.getUTCHours() == 0) {
+                            if (date1.getUTCMinutes() - date2.getUTCMinutes() > 0) {
                                 arr[counter] = i;
                                 counter++;
+                            }
+                            else if (date1.getUTCMinutes() - date2.getUTCMinutes() == 0) {
+                                if (date1.getUTCSeconds() - date2.getUTCSeconds() > 0) {
+                                    arr[counter] = i;
+                                    counter++;
+                                }
                             }
                         }
                     }
@@ -50,11 +55,13 @@ for (let i = 0; i < obj.length; i++) {
             }
         }
     }
+
+    for (let i = 1; i <= arr.length && i <= 3; i++) {
+        document.getElementById("events-table" + i).style.visibility = "visible";
+        document.getElementById("events-table" + i + "-header").innerHTML = obj[arr[i - 1]].title;
+        document.getElementById("events-table" + i + "-date").innerHTML = obj[arr[i - 1]].date;
+        document.getElementById("events-table" + i + "-desc").innerHTML = obj[arr[i - 1]].description;
+    }
 }
 
-for(let i = 1; i <= arr.length && i <= 3; i++){
-    document.getElementById("events-table" + i).style.visibility = "visible";
-    document.getElementById("events-table" + i + "-header").innerHTML = obj[arr[i-1]].title;
-    document.getElementById("events-table" + i + "-date").innerHTML = obj[arr[i-1]].date;
-    document.getElementById("events-table" + i + "-desc").innerHTML = obj[arr[i-1]].description;
-}
+foo();
